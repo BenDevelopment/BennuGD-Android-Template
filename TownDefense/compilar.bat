8e8d3574215df34b65b65e0e495bf22d95756b1a
unknown <bhoffmann68@gmail.com>
2015-07-20 22:34:00 +0200

    TownDefense project removed

--- TownDefense/compilar.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd src
-echo Build in progress...
-..\..\bennu-win\bgdc -D -g main.prg
-move main.dcb ..
-cd ..
-..\bennu-win\bgdi main.dcb
-
-exit
--- TownDefense/compilarfpgs.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-set bits=%1
-if "%bits%"==""; set bits=32
-call ..\scripts\compilarfpgs.bat %bits%

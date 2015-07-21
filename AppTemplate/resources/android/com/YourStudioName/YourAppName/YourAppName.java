7da8d9355504e00b0db38236d4ab4c6b9fe9f0e2
Benjamin Hoffmann <b.hoffmann@adhoc-gti.com>
2015-07-21 17:40:49 +0200

    Android version changed to 19. New Keystore created

--- AppTemplate/resources/android/com/YourStudioName/YourAppName/YourAppName.java
+++ AppTemplate/resources/android/com/YourStudioName/YourAppName/YourAppName.java
@@ -2,19 +2,31 @@ package com.YourStudioName.YourAppName;
 
 import org.libsdl.app.SDLActivity;
 import android.os.*;
-// import android.view.*;
+import android.view.*;
 
 public class YourAppName extends SDLActivity {
     protected void onCreate(Bundle savedInstanceState) {
-		// getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+		// getWindow().getDecorView().setSystemUiVisibility( View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 // | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 // | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                // | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                // | View.SYSTEM_UI_FLAG_FULLSCREEN
-                // | View.SYSTEM_UI_FLAG_IMMERSIVE);
+                // | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                // | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                // | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+		getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         super.onCreate(savedInstanceState);
     }
     
+	@Override
+    protected void onResume() {
+        super.onResume();
+		getWindow().getDecorView().setSystemUiVisibility( View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+	
     protected void onDestroy() {
         super.onDestroy();
     }
